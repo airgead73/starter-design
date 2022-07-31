dea609724ebcfe0220653dce34dc9cc9c68ec7d9
Dan Moneypenny <catullus1973@gmail.com>
2022-07-31 06:39:51 -0700

    update

--- app/public/bundle.js
+++ app/public/bundle.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const initMenu = () => {
-  const navBtn = document.getElementById('nav__btn');
+  const navBtn = document.getElementById('menu__btn');
   const navList = document.getElementById('nav__list');
   
   navBtn.addEventListener('click', function(e) {
--- assets/scripts/modules/menu.js
+++ assets/scripts/modules/menu.js
@@ -1,5 +1,5 @@
 const initMenu = () => {
-  const navBtn = document.getElementById('nav__btn');
+  const navBtn = document.getElementById('menu__btn');
   const navList = document.getElementById('nav__list');
   
   navBtn.addEventListener('click', function(e) {
