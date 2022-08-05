74bb3dd3c0fe4b4c3fcc9e4058456ac53eedea11
Dan Moneypenny <catullus1973@gmail.com>
2022-08-05 07:40:48 -0700

    add icon toggle

--- app/public/bundle.js
+++ app/public/bundle.js
@@ -3,13 +3,26 @@
 
 const initMenu = () => {
   console.log('init menu');
-  // const navBtn = document.getElementById('menu__btn');
-  // const navList = document.getElementById('nav__list');
+  const navBtn = document.getElementById('nav__menu');
+  const navList = document.getElementById('nav__list');
+  const navIcon = document.getElementById('nav__icon');
+
+  const toggleMenuIcon = () => {
+    const isHamburger = navIcon.classList.contains('fa-bars');
+    if(isHamburger) {
+      navIcon.classList.remove('fa-bars');
+      navIcon.classList.add('fa-xmark');
+    } else {
+      navIcon.classList.remove('fa-xmark');
+      navIcon.classList.add('fa-bars');
+    }
+  };
   
-  // navBtn.addEventListener('click', function(e) {
-  //   e.preventDefault();
-  //   navList.classList.toggle('show');
-  // });
+  navBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    navList.classList.toggle('show');
+    toggleMenuIcon();
+  });
   
 };
 
