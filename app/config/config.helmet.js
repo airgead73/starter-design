ae71bff2d76b1274fca55eecc6510266ba1a5962
Dan Moneypenny <catullus1973@gmail.com>
2022-08-05 04:20:16 -0700

    add fontawesome

--- app/config/config.helmet.js
+++ app/config/config.helmet.js
@@ -5,8 +5,8 @@ const policies = {
     "font-src": ["'self'", "https://fonts.googleapis.com", "https://fonts.gstatic.com","https://cdn.jsdelivr.net"],
     "style-src-elem": ["'self'", "https://fonts.googleapis.com", "https://fonts.gstatic.com", "https://cdn.jsdelivr.net"],
     "style-src": ["'self'", "'unsafe-inline'"],
-    "script-src-elem": ["'self'", "https://cdn.jsdelivr.net"],
-    "script-src": ["'self'", "https://cdn.jsdelivr.net"]
+    "script-src-elem": ["'self'", "https://cdn.jsdelivr.net", "https://kit.fontawesome.com/c2a0ddd321.js"],
+    "script-src": ["'self'", "https://cdn.jsdelivr.net", "https://kit.fontawesome.com/c2a0ddd321.js"]
   }
 }
 
