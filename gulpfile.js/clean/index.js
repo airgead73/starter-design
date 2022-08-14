dd21f96e4c4710401a947dca7a4f34b4f1bf4425
Dan Moneypenny <catullus1973@gmail.com>
2022-08-14 03:40:41 -0700

    add cldan constants

--- gulpfile.js/clean/index.js
+++ gulpfile.js/clean/index.js
@@ -1,12 +1,6 @@
 const clean = require('./clean');
-const { 
-  CONSTANTS_CLEAN,
-  CONSTANTS_PRODUCTION
- } = require('./constants');
 
 //exports.clean = clean;
 module.exports = {
   clean,
-  CONSTANTS_CLEAN,
-  CONSTANTS_PRODUCTION
 }
