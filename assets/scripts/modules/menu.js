e1ff2e98a7daac399543a10072da8adfd549ccfb
Dan Moneypenny <catullus1973@gmail.com>
2022-09-10 21:17:55 -0700

    update

--- assets/scripts/modules/menu.js
+++ assets/scripts/modules/menu.js
@@ -1,24 +1,25 @@
 const initMenu = () => {
-  const navBtn = document.getElementById('nav__btn');
-  const navIcon = document.getElementById('nav__icon');
-  const sideNavigation = document.getElementById('side-navigation');
+  console.log('init menu');
+  // const navBtn = document.getElementById('nav__btn');
+  // const navIcon = document.getElementById('nav__icon');
+  // const sideNavigation = document.getElementById('side-navigation');
 
-  const toggleMenuIcon = () => {
-    const isHamburger = navIcon.classList.contains('fa-bars');
-    if(isHamburger) {
-      navIcon.classList.remove('fa-bars');
-      navIcon.classList.add('fa-xmark');
-    } else {
-      navIcon.classList.remove('fa-xmark');
-      navIcon.classList.add('fa-bars');
-    }
-  }
+  // const toggleMenuIcon = () => {
+  //   const isHamburger = navIcon.classList.contains('fa-bars');
+  //   if(isHamburger) {
+  //     navIcon.classList.remove('fa-bars');
+  //     navIcon.classList.add('fa-xmark');
+  //   } else {
+  //     navIcon.classList.remove('fa-xmark');
+  //     navIcon.classList.add('fa-bars');
+  //   }
+  // }
   
-  navBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    sideNavigation.classList.toggle('open');
-    toggleMenuIcon();
-  });
+  // navBtn.addEventListener('click', function(e) {
+  //   e.preventDefault();
+  //   sideNavigation.classList.toggle('open');
+  //   toggleMenuIcon();
+  // });
   
 }
 
--- gulpfile.js/test/scss.js
+++ gulpfile.js/test/scss.js
@@ -13,7 +13,7 @@ function scss() {
   return (src(C.SRC, { sourcemaps: true }))
     .pipe(sass())
     .pipe(postcss([autoprefixer(), combinemq()])) 
-    .pipe(purgecss({ content: C.CONTENT, fontFace: true, safelist: ['fa-xmark']}))
+    .pipe(purgecss({ content: C.CONTENT, fontFace: true, safelist: ['fa-xmark', 'open']}))
     .pipe(rename(C.OUTPUT))
     .pipe(dest(C.DEST, { sourcemaps: '.'}))
 }
