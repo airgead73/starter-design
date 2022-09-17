f1ca7df0efaa08324caaa3c2b0ea7b82fb8edcc3
Dan Moneypenny <catullus1973@gmail.com>
2022-09-16 23:05:19 -0700

    update

--- gulpfile.js/constants/DEVELOPMENT.js
+++ gulpfile.js/constants/DEVELOPMENT.js
@@ -6,18 +6,21 @@ const DEVELOPMENT_JS = {
   MAP: 'bundle.js.map',
   DEST: './app/public'
 };
+
 const DEVELOPMENT_SCSS = {
   SRC: 'assets/scss/index.scss',
   OUTPUT: 'main.css',
   MAP: 'main.css.map',
   DEST: 'app/public'
 };
+
 const DEVELOPMENT_ICONS = {
   SRC: 'assets/icons/index.scss',
   OUTPUT: 'icons.css',
   MAP: 'icon.css.map',
   DEST: 'app/public'
 };
+
 const DEVELOPMENT_WATCH = {
   ARRAY: ['assets/scss/index.scss','assets/scss/**/*.scss', 'assets/icons/index.scss', 'assets/icons/**/*.scss', 'assets/scripts/**/*.js']
 };
