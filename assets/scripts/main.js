50d7394ebb121d282bdac172bfbe2b068c429f34
Dan Moneypenny <catullus1973@gmail.com>
2022-10-08 22:23:49 -0700

    update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,7 @@
 import { initMenu } from './modules/index';
+import { initForms } from './modules/index';
+
+const forms = Array.from(document.querySelectorAll('.form'));
 
 initMenu();
+if(forms) initForms(forms);
--- assets/scripts/modules/OLDfetch.js
+++ assets/scripts/modules/OLDfetch.js
@@ -1,30 +1,43 @@
-import { getAttrs } from './utils';
+const getAttrs = ($target) => {
+
+  const attrs = new Object;
+
+  const attrNames = $target.getAttributeNames();
+
+  attrNames.forEach(name => {    
+    attrs[name] = $target.getAttribute(name);    
+  });
+
+  console.log(attrs);  
+
+}
 
 const getBody = ($target) => {
+
   let body = new Object;
+
   let formElements = Array.from($target.elements);
   formElements = formElements.filter(element => {
-    if(element.hasAttribute('name')) return element;
+    if(element.hasAttribute('name')) {
+      return element;
+    }
   });
 
   formElements.forEach(element => {
     const property = element.getAttribute('name');
-    const value = $target[property].value;
+    const value = $form[property].value;
     body[property] = value;
   });
 
   body = JSON.stringify(body);
 
-  console.log(body);
+  return body;
+
 }
-
-
 
 const apiFetch = ($target) => {
 
-  const { action, method }= getAttrs($target);
-
-  console.log(method)
+ getAttrs($target);
 
 }
 
