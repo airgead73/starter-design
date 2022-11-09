6af495c7708d4112f7e477ba919bd8987b6943db
Dan Moneypenny <catullus1973@gmail.com>
2022-11-09 05:41:30 -0800

    add item unit

---
+++ app/units/items/index.js
@@ -0,0 +1,7 @@
+const { itemApi } = require('./item.routes');
+const Item = require('./item');
+
+module.exports = {
+  itemApi,
+  Item
+}
--- app/units/router.js
+++ app/units/router.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 const { authorAPI, authorCLIENT } = require('./authors/author.routes');
+const { itemAPI, itemCLIENT } = require('./items/item.routes');
 
 router.use('/authors', authorCLIENT);
+router.use('/items', itemCLIENT);
 
 router.use('/api/authors', authorAPI);
+router.use('/api/items', itemAPI);
 
 module.exports = router;
